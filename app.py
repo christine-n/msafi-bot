8e3a2cdd23cfa9c7f0a1107ec70683734b375102
christinendumba@gmail.com <christine>
2022-08-05 16:08:01 +0300

    update message

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def respond():
    if text == "/start":
        # print the welcoming message
        bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       Welcome to msafi bot.
        """
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
